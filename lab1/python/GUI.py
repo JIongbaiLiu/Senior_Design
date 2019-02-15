02993695a85d78f153f7c5c935e115c566d4351c
Cooper Bell <cooper-bell@uiowa.edu>
2019-02-15 11:08:37 -0600

    integrating SMS eservice and GUI buttons

--- lab1/python/GUI.py
+++ lab1/python/GUI.py
@@ -14,7 +14,7 @@ import tkinter
 from tkinter.ttk import Frame
 
 
-class serialPlot:
+class serialManagement:
     def __init__(self, serialPort='/dev/cu.usbserial-1440', serialBaud=38400, plotLength=100, dataNumBytes=2):
         self.port = serialPort
         self.baud = serialBaud
@@ -69,20 +69,51 @@ class serialPlot:
 
 
 # ---------------------------------------------------------------------------------------------------------------------
+# TODO: refactor all this with the current GUI that's being set up in main
 class Window(Frame):
-    def __init__(self, figure, master, SerialReference):
-        Frame.__init__(self, master)
+    def __init__(self, fig, root, max_temp, min_temp, phone_nums):
+        Frame.__init__(self, root)
         self.entry = None
         self.setPoint = None
-        self.master = master
-        self.serialReference = SerialReference
-        self.initWindow(figure)
+        self.master = root
+        self.initWindow(fig)
+        self.max_temp = max_temp
+        self.min_temp = min_temp
+        self.frame = Frame(root)
+        self.receiving_number = phone_nums[0]
+        self.sending_number = phone_nums[1]
 
-    def initWindow(self, figure):
+
+    def initWindow(self, fig):
         self.master.title("Real Time Temperature")
-        canvas = FigureCanvasTkAgg(figure, master=self.master)
-        canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        canvas = FigureCanvasTkAgg(fig, master=self.master)  # A tk.DrawingArea.
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=0, column=1)
+        self.master.columnconfigure(0, weight=1)
+        self.frame.grid(row=0, column=0, sticky="n")
 
+        maxTemp_label = tkinter.Label(self.frame, text="Max Temp").grid(row=1, column=0, sticky="w")
+        maxTemp_entry = tkinter.Entry(self.frame).grid(row=1, column=1, sticky=tkinter.E + tkinter.W)
+        maxTemp_Button = tkinter.Button(self.frame, text="Update").grid(row=1, column=3, sticky="we")
+
+        minTemp_label = tkinter.Label(self.frame, text="Min Temp").grid(row=2, column=0, sticky="w")
+        minTemp_entry = tkinter.Entry(self.frame).grid(row=2, column=1, sticky=tkinter.E)
+        minTemp_Button = tkinter.Button(self.frame, text="Update").grid(row=2, column=3, sticky="we")
+
+        phone_label = tkinter.Label(self.frame, text="Phone #").grid(row=3, column=0, sticky="w")
+        phone_entry = tkinter.Entry(self.frame).grid(row=3, column=1, sticky=tkinter.E + tkinter.W)
+        phone_Button = tkinter.Button(self.frame, text="Update").grid(row=3, column=3, sticky="we")
+
+        Button4 = tkinter.Button(self.frame, text="Turn off LEDs").grid(row=4, column=0, sticky="we")
+
+    # def update_max:
+    #     # self.max_temp = maxTemp_entry or something like this
+    #
+    # def update_min:
+    #     #stuff
+    #
+    # def update_phone_number:
+    #     #
 
 # ---------------------------------------------------------------------------------------------------------------------
 def animate(self, sp, lines, lineValueText, lineLabel):
@@ -90,14 +121,13 @@ def animate(self, sp, lines, lineValueText, lineLabel):
     # this is smoothing out the arduino's random data bc I'm too lazy to change the code in the arduino
     # new_value = ((5/20)*(value-30)) + 30
     if value > 34:
-        new_value = None
-        sp.data.appendleft(new_value)  # we get the latest data point and append it to our array
+        value = None
+        sp.data.appendleft(value)  # we get the latest data point and append it to our array
         lineValueText.set_text('Sensor Unplugged')
     else:
         sp.data.appendleft(value)  # we get the latest data point and append it to our array
         lines.set_data(range(sp.plotMaxLength), sp.data)
         lineValueText.set_text(lineLabel + ' = ' + str(value))
-
 
 # ---------------------------------------------------------------------------------------------------------------------
 def main():
@@ -106,16 +136,19 @@ def main():
     baudRate = 38400
     maxPlotLength = 101  # number of points in x-axis
     dataNumBytes = 4  # number of bytes of 1 data point
-    sp = serialPlot(portName, baudRate, maxPlotLength, dataNumBytes)  # initializes all required variables
-    sp.readSerialStart()  # starts background thread
+    sm = serialManagement(portName, baudRate, maxPlotLength, dataNumBytes)  # initializes all required variables
+    sm.readSerialStart()  # starts background thread
 
     # setup texting service
-    sending_number = '+15156196749'  # Twilio's number
-    receiving_number = '+15153710142'  # Your phone number
-    texting_service = send_sms.TextSMS(sending_number, receiving_number)
+    phone_nums = ['+15156196749', '+15153710142']
+    # sending_number = '+15156196749'  # Twilio's number
+    # receiving_number = '+15153710142'  # Your phone number
+    max_temp = 36
+    min_temp = 24
+    # texting_service = send_sms.TextSMS()
 
     # setup plot
-    pltInterval = 1000  # Period at which the plot animation updates [ms]
+    pltInterval = 1000  # Period at which the plot animation update in ms
     xmin = 0
     xmax = maxPlotLength - 1  # The - 1 allows the line to touch the edge of the plot
     ymin = 10
@@ -131,41 +164,42 @@ def main():
 
     # Tkinter's GUI
     root = tkinter.Tk()
-    # app = Window(fig, root, sp)
 
-    canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-    canvas.draw()
-    # canvas.get_tk_widget().pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
-    canvas.get_tk_widget().grid(row=0, column=1)
-    root.columnconfigure(0, weight=1)
-    root.title("Real Time Temperature")
+    app = Window(fig, root, max_temp, min_temp, phone_nums)
 
-    frame = Frame(root)
-    frame.grid(row=0, column=0, sticky="n")
+    # canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+    # canvas.draw()
+    # # canvas.get_tk_widget().pack(side=tkinter.RIGHT, fill=tkinter.BOTH, expand=1)
+    # canvas.get_tk_widget().grid(row=0, column=1)
+    # root.columnconfigure(0, weight=1)
+    # # root.title("Real Time Temperature")
+    #
+    # frame = Frame(root)
+    # frame.grid(row=0, column=0, sticky="n")
 
-    maxTemp_label = tkinter.Label(frame, text="Max Temp").grid(row=1, column=0, sticky="w")
-    maxTemp_entry = tkinter.Entry(frame).grid(row=1, column=1, sticky=tkinter.E + tkinter.W)
-    maxTemp_Button = tkinter.Button(frame, text="Update").grid(row=1, column=3, sticky="we")
-
-    minTemp_label = tkinter.Label(frame, text="Min Temp").grid(row=2, column=0, sticky="w")
-    minTemp_entry = tkinter.Entry(frame).grid(row=2, column=1, sticky=tkinter.E)
-    minTemp_Button = tkinter.Button(frame, text="Update").grid(row=2, column=3, sticky="we")
-
-    phone_label = tkinter.Label(frame, text="Phone #").grid(row=3, column=0, sticky="w")
-    phone_entry = tkinter.Entry(frame).grid(row=3, column=1, sticky=tkinter.E + tkinter.W)
-    phone_Button = tkinter.Button(frame, text="Update").grid(row=3, column=3, sticky="we")
-
-    Button4 = tkinter.Button(frame, text="Turn off LEDs").grid(row=4, column=0, sticky="we")
+    # maxTemp_label = tkinter.Label(frame, text="Max Temp").grid(row=1, column=0, sticky="w")
+    # maxTemp_entry = tkinter.Entry(frame).grid(row=1, column=1, sticky=tkinter.E + tkinter.W)
+    # maxTemp_Button = tkinter.Button(frame, text="Update", command=update_max).grid(row=1, column=3, sticky="we")
+    #
+    # minTemp_label = tkinter.Label(frame, text="Min Temp").grid(row=2, column=0, sticky="w")
+    # minTemp_entry = tkinter.Entry(frame).grid(row=2, column=1, sticky=tkinter.E)
+    # minTemp_Button = tkinter.Button(frame, text="Update").grid(row=2, column=3, sticky="we")
+    #
+    # phone_label = tkinter.Label(frame, text="Phone #").grid(row=3, column=0, sticky="w")
+    # phone_entry = tkinter.Entry(frame).grid(row=3, column=1, sticky=tkinter.E + tkinter.W)
+    # phone_Button = tkinter.Button(frame, text="Update").grid(row=3, column=3, sticky="we")
+    #
+    # Button4 = tkinter.Button(frame, text="Turn off LEDs").grid(row=4, column=0, sticky="we")
 
     lineLabel = "Temperature (\u00b0C)"
     lines = ax.plot([], [], label=lineLabel)[0]
     lineValueText = ax.text(0.50, 0.90, '', transform=ax.transAxes)
-    anim = animation.FuncAnimation(fig, animate, fargs=(sp, lines, lineValueText, lineLabel), interval=pltInterval)
+    anim = animation.FuncAnimation(fig, animate, fargs=(sm, lines, lineValueText, lineLabel),interval=pltInterval)
 
     plt.legend(loc="upper left")
     root.mainloop()
 
-    sp.close()
+    sm.close()
 
 
 if __name__ == '__main__':
